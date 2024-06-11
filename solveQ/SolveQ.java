1a857f8295a2936955a9c253a0858c30bb7847bf
duncanimi <duncantomke1@gmail.com>
2024-06-11 13:24:29 +0200

    aufgabe 4 ohne schmerz nur mit herz

--- solveQ/SolveQ.java
+++ solveQ/SolveQ.java
@@ -2,6 +2,39 @@ package solveQ;
 
 public class SolveQ {
     public static void main(String[] args) {
-        
+    }
+
+    private static boolean SolveQ(int[][] arr, int row, int col) {
+        if (row == 0)
+            return true;
+        // horizontalchecker
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[row][i] == 1)
+                return false;
+        }
+        // vertikalchecker possible von malte
+        for (int i = 0; i <= row; i++) {
+            if (arr[i][col] == 1)
+                return false;
+        }
+        // linke Diagonale
+        int i = row - 1;
+        int j = col - 1;
+        while (i >= 0 && j >= 0) {
+            if (arr[i][j] == 1)
+                return false;
+            i--;
+            j--;
+        }
+        // rechte Diagonale
+        i = row - 1;
+        j = col + 1;
+        while (i >= 0 && j < arr.length) {
+            if (arr[i][j] == 1)
+                return false;
+            i--;
+            j++;
+        }
+        return true;
     }
 }
