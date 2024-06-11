17e422f663c18b166719a5141235c070129a0724
MrCake4 <winstonjreichelt@gmail.com>
2024-06-11 12:53:14 +0200

    Chessboard perfected ++

--- src/Chessboard.java
+++ src/Chessboard.java
@@ -3,6 +3,16 @@ package src;
 public class Chessboard {
     String[][] board = new String[8][8];
 
+    public Chessboard(){
+        for(int h = 0; h < 8; h++) {
+            for(int v = 0; v < 8; v++){
+                if (board[h][v] == null) {
+                    board[h][v] = "0";
+                }
+            }
+            }
+    }
+
     public void putPiece(int h, int v, String piece){     // horizontal, vertikal, piece
         board[h][v] = piece;
     }
@@ -13,14 +23,14 @@ public class Chessboard {
         for(int h = 0; h < 8; h++) {
             res += h + ". ";
             for(int v = 0; v < 8; v++){
-                if (board[h][v] == null) {
+                if (board[h][v] == "0") {
                     board[h][v] = "_";
                 }
-                res += board[h][v];
+                res += board[h][v] + " ";
             }
             res += "\n";
         }
-        res += "#. abcdefgh";
+        res += "#. a b c d e f g h";
         return res;
     }
 }
